fcf1931b8f8f5647a60efa0670c3a927f394bdec
coconut750750 <sourbrandon@gmail.com>
2017-12-02 23:29:30 -0600

    view holder swiping working

--- app/src/main/java/edu/illinois/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/finalproject/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.illinois.finalproject;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mViewPager.setAdapter(new PageSwipeAdapter(getSupportFragmentManager()));
     }
 }
